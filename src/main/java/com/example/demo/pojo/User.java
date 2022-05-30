7ead279cdcd81dacca3929285b8abd37fffdc85b
RenBoQing <2098510757@qq.com>
2022-05-30 15:04:52 +0800

    aa

---
+++ src/main/java/com/example/demo/pojo/User.java
@@ -0,0 +1,15 @@
+package com.example.demo.pojo;
+
+import lombok.Data;
+
+/**
+ * @author RenBoQing
+ * @date 2022年05月30日 15:03
+ * @Description  测试用户类
+ */
+@Data
+public class User {
+    private Long id;
+    private String username;
+    private String password;
+}
